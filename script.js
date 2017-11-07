9e339cb76b15841fa25b3d7956931f5ec571247f
Hilary Claire <hilary.wilson2563@gmail.com>
2017-11-06 23:12:14 -0500

    added index

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 $(document).ready(function() {
   $(".available").click(function () {
-			$(".popUpForm").css("display","block");
-			$(".tableNumber").text("Table Number: " + this.innerHTML);
-			$(".available").css("z-index","-20");
-			$(this).toggleClass("reserved");
-		$(".submitInfo").click(function(){
-  			$(".popUpForm").css("display","none");
-  			$(".available").css("z-index","0");
+      $(".popUpForm").css("display","block");
+      $(".tableNumber").text("Table Number: " + this.innerHTML);
+      $(".available").css("z-index","-20");
+      $(".index").css("display", "none");
+      $(this).toggleClass("reserved");
+    $(".submitInfo").click(function(){
+        $(".popUpForm").css("display","none");
+        $(".available").css("z-index","0");
+        $(".index").css("display", "flex");
       })
-	});
+  });
   $(".material-icons").click(function(){
-  	$(".popUpForm").css("display","none");
-  	$(".available").css("z-index","0");
-  	$(".available").toggleClass(".available");
+    $(".popUpForm").css("display","none");
+    $(".available").css("z-index","0");
+    $(".available").toggleClass(".available");
+    $(".index").css("display", "flex");
   });
   $(".available").mouseenter(function () {
     $(this).fadeTo(400, .25); 
@@ -22,4 +25,3 @@ $(document).ready(function() {
     });
   $("input")
 });
-
